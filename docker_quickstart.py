cb73896a3b708f85f0de98f3bbc08d2892a850b8
Bryan Chua <me@bryanchua.com>
2018-04-07 13:01:12 +0800

    Testing ci cd #2

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized. Hello World!!'
+    return 'Flask Dockerized. Hello World! 2!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
