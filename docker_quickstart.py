f787e769c2dc09bee1659fee4bbc809501d83cf3
Bryan Chua <me@bryanchua.com>
2018-05-16 14:55:05 +0800

    Adding new endpoint for fargate

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Flask Dockerized. Hello World! 3!'
 
+@app.route('/flaskfargatedemo')
+def hello_fargate():
+    return 'Hell Fargate!'
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
