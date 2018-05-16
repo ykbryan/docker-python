90ba3647e0ce5bed58426bf88010276d079bcc73
Bryan Chua <me@bryanchua.com>
2018-05-16 20:48:50 +0800

    Removing useless endpoints

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -3,19 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized. Hello World, 3!'
-
-@app.route('/flaskfargatedemo')
-def hello_flaskfargatedemo():
-    return 'Hello flaskfargatedemo!'
+    return 'Hello World!'
 
 @app.route('/fargate')
 def hello_fargate():
     return 'Hello Fargate!'
-
-@app.route('/api')
-def hello_api():
-    return 'Hello API Endpoint!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
